e345c38181f2319a12b36f0f8056ed9734d4c4a5
Marina Celeste Chavez <94614751+marinacchavez89@users.noreply.github.com>
2025-03-25 19:28:22 -0300

    Se crea proyecto TP1 con sus dos paquetes + clase Main de prueba. Se añaden usuarios auth

---
+++ TP1_GRUPO_8/src/Ejercicio1/Main.java
@@ -0,0 +1,11 @@
+package Ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Esta clase en realidad es la que debería llamarse "Principal"
+		// La cree y le puse "Main" para probar que el proyecto se generó correctamente.
+		 System.out.println("¡Proyecto TP1_GRUPO8 creado con éxito!");
+	}
+
+}
