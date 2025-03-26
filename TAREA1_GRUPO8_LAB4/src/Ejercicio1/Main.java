5804afb20cdd380e8e631b11d2d506b8c9fda81c
juancruzgauna <juancruzgauna97@gmail.com>
2025-03-26 19:41:12 -0300

    Agregado de atributos utilizando el metodo de encapsulamiento. Se aplican getters y setters con el agregado automatico explicado en clase

--- TAREA1_GRUPO8_LAB4/src/Ejercicio1/Main.java
+++ TAREA1_GRUPO8_LAB4/src/Ejercicio1/Main.java
@@ -6,6 +6,13 @@ public class Main {
 		// Esta clase en realidad es la que debería llamarse "Principal"
 		// La cree y le puse "Main" para probar que el proyecto se generó correctamente.
 		 System.out.println("¡Proyecto TP1_GRUPO8 creado con éxito!");
+		 
+		 // Prueba de funcionamiento correcto de atributo.
+		 Empleado empleado1 = new Empleado();
+
+		 empleado1.setEdad(22);
+		 System.out.println(empleado1.getEdad());
+		
 	}
 
 }
