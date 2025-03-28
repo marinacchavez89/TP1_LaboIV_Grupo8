56c73dd0884aea576a89590418908f6cb36ce043
Marina Celeste Chavez <94614751+marinacchavez89@users.noreply.github.com>
2025-03-28 18:09:45 -0300

    Se agrega método en clase Empleado - devuelveProximoID()

--- TAREA1_GRUPO8_LAB4/src/Ejercicio1/Empleado.java
+++ TAREA1_GRUPO8_LAB4/src/Ejercicio1/Empleado.java
@@ -6,24 +6,22 @@ public class Empleado {
 	private String nombre;
 	private int edad;
 	
-	// variable de la clase y no de los objetos
+	// Variable de la clase y no de los objetos
 	private static int cont = 1000;
-	// constructor
-	public Empleado()
-	{
+	
+	// Primer constructor
+	public Empleado() {
 	 this.id = cont++;
 	 this.nombre = "sin nombre";
 	 this.edad = 99;
 	}
 	
 	// Segundo constructor (recibe nombre y edad como parámetros)
-		public Empleado(String nombre, int edad) {
-			this.id = cont++; // Se mantiene el autoincremento del id
-			this.nombre = nombre;
-			this.edad = edad;
-		}
-
-	
+	public Empleado(String nombre, int edad) {
+		this.id = cont++; // Se mantiene el autoincremento del id
+		this.nombre = nombre;
+		this.edad = edad;
+	}	
 	
 	public int getId() {
 		return id;
@@ -41,5 +39,11 @@ public class Empleado {
 		this.edad = edad;
 	}
 	
+	public static int devuelveProximoID(int idAnterior) {
+		int idProximo;
+		idAnterior++;
+		idProximo = idAnterior;
+		return idProximo;
+	}
 	
 }
--- TAREA1_GRUPO8_LAB4/src/Ejercicio1/Main.java
+++ TAREA1_GRUPO8_LAB4/src/Ejercicio1/Main.java
@@ -3,8 +3,8 @@ package Ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		// Esta clase en realidad es la que debería llamarse "Principal"
-		// La cree y le puse "Main" para probar que el proyecto se generó correctamente.
+		 // Esta clase en realidad es la que debería llamarse "Principal"
+		 // La cree y le puse "Main" para probar que el proyecto se generó correctamente.
 		 System.out.println("¡Proyecto TP1_GRUPO8 creado con éxito!");
 		 
 		 /*Prueba de funcionamiento correcto de atributo.
@@ -12,13 +12,21 @@ public class Main {
 		 empleado1.setEdad(22);
 		 System.out.println(empleado1.getEdad());*/
 		 
-		// Prueba del constructor vacio con valores inciales
+		 // Prueba del constructor vacio con valores inciales
 		 Empleado empleado1 = new Empleado();
-	     System.out.println("Empleado 1: ID = " + empleado1.getId() + ", Nombre = " + empleado1.getNombre() + ", Edad = " + empleado1.getEdad());
-
-	    // Prueba del constructor con parametros nombre y edad 
-	     Empleado empleado2 = new Empleado("Camila", 30);
-	     System.out.println("Empleado 2: ID = " + empleado2.getId() + ", Nombre = " + empleado2.getNombre() + ", Edad = " + empleado2.getEdad());
+		 System.out.println("Empleado 1: ID = " + empleado1.getId() + ", Nombre = " + empleado1.getNombre() + ", Edad = " + empleado1.getEdad());
+		
+		 // Prueba del constructor con parametros nombre y edad
+		 Empleado empleado2 = new Empleado("Camila", 30);
+		 System.out.println("Empleado 2: ID = " + empleado2.getId() + ", Nombre = " + empleado2.getNombre() + ", Edad = " + empleado2.getEdad());
+		
+		 // Prueba1 funcionamiento devuelveProximoID()
+		 Empleado empleado3 = new Empleado();
+		 System.out.println("Empleado 3: ID = " + empleado3.getId() + " El próximo ID será el " + Empleado.devuelveProximoID(empleado3.getId()));
+		 // Prueba12 funcionamiento devuelveProximoID()
+		 Empleado empleado4 = new Empleado();
+		 System.out.println("Empleado 4: ID = " + empleado4.getId() + " El próximo ID será el " + Empleado.devuelveProximoID(empleado4.getId()));
+	     
 	}
 
 }
